dd3eaa1a0b31ce8245c9b93722cfd174d2045e6d
danischm <danischm@cisco.com>
2023-12-01 11:36:38 +0100

    Add network access authorization global exception rule resource

---
+++ examples/resources/ise_network_access_authorization_global_exception_rule/resource.tf
@@ -0,0 +1,14 @@
+resource "ise_network_access_authorization_global_exception_rule" "example" {
+  name                      = "Rule1"
+  default                   = false
+  rank                      = 0
+  state                     = "enabled"
+  condition_type            = "ConditionAttributes"
+  condition_is_negate       = false
+  condition_attribute_name  = "Location"
+  condition_attribute_value = "All Locations"
+  condition_dictionary_name = "DEVICE"
+  condition_operator        = "equals"
+  profiles                  = ["PermitAccess"]
+  security_group            = "BYOD"
+}
