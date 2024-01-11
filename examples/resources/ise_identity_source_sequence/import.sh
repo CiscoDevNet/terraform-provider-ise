e86a92d922f0e5de8dc27eba6dc9d7f516b78a49
danischm <danischm@cisco.com>
2024-01-11 21:21:35 +0100

    Add identity source sequence resource

---
+++ examples/resources/ise_identity_source_sequence/import.sh
@@ -0,0 +1,1 @@
+terraform import ise_identity_source_sequence.example "76d24097-41c4-4558-a4d0-a8c07ac08470"
---
+++ examples/resources/ise_identity_source_sequence/resource.tf
@@ -0,0 +1,12 @@
+resource "ise_identity_source_sequence" "example" {
+  name                               = "Sequence1"
+  description                        = "My identity source sequence"
+  break_on_store_fail                = true
+  certificate_authentication_profile = "Global_Certificate"
+  identity_sources = [
+    {
+      name  = "Internal Users"
+      order = 1
+    }
+  ]
+}
