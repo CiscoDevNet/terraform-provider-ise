0b7051f664f8e7583169a39081803cc8b69f9749
Kuba Mazurkiewicz <kmazurki@cisco.com>
2024-01-25 12:52:30 +0100

    removed get_group_by_domain_examples

--- examples/data-sources/ise_active_directory_get_groups_by_domain/data-source.tf
+++
@@ -1,4 +0,0 @@
-data "ise_active_directory_get_groups_by_domain" "example" {
-  id            = "73808580-b6e6-11ee-8960-de6d7692bc40"
-  join_point_id = "73808580-b6e6-11ee-8960-de6d7692bc40"
-}
--- examples/resources/ise_active_directory_get_groups_by_domain/resource.tf
+++
@@ -1,9 +0,0 @@
-resource "ise_active_directory_get_groups_by_domain" "example" {
-  join_point_id = "73808580-b6e6-11ee-8960-de6d7692bc40"
-  additional_data = [
-    {
-      name  = "domain"
-      value = "cisco.local"
-    }
-  ]
-}
