46e1307e4ad0a760ddd011b8fa605bfe6c1191d8
Rafal Slominski <rslominski@gmail.com>
2025-04-04 16:25:29 +0200

    cleanup #2

--- examples/resources/ise_network_access_authorization_rule_update_ranks/resource.tf
+++ examples/resources/ise_network_access_authorization_rule_update_ranks/resource.tf
@@ -1,4 +1,4 @@
-resource "ise_network_access_authorization_rule_update_rank_bulk" "example" {
+resource "ise_network_access_authorization_rule_update_ranks" "example" {
   policy_set_id = "d82952cb-b901-4b09-b363-5ebf39bdbaf9"
   rules = [
     {
