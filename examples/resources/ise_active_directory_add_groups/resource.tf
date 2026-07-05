61a79f410a6cac8a0e75e401ae4cb04103dd4724
camschae <camschae@cisco.com>
2026-07-04 20:34:15 -0400

    style: restore correct docs from main and run go generate ./...

--- examples/resources/ise_active_directory_add_groups/resource.tf
+++ examples/resources/ise_active_directory_add_groups/resource.tf
@@ -1,14 +1,14 @@
 resource "ise_active_directory_add_groups" "example" {
-  join_point_id = "73808580-b6e6-11ee-8960-de6d7692bc40"
-  name = "cisco.local"
-  description = "My AD join point"
-  domain = "cisco.local"
-  ad_scopes_names = "Default_Scope"
+  join_point_id              = "73808580-b6e6-11ee-8960-de6d7692bc40"
+  name                       = "cisco.local"
+  description                = "My AD join point"
+  domain                     = "cisco.local"
+  ad_scopes_names            = "Default_Scope"
   enable_domain_allowed_list = true
   groups = [
     {
       name = "cisco.local/operators"
-      sid = "S-1-5-32-548"
+      sid  = "S-1-5-32-548"
       type = "GLOBAL"
     }
   ]
--- examples/resources/ise_device_admin_condition/resource.tf
+++ examples/resources/ise_device_admin_condition/resource.tf
@@ -1,10 +1,10 @@
 resource "ise_device_admin_condition" "example" {
-  name = "Cond1"
-  description = "My description"
-  condition_type = "LibraryConditionAttributes"
-  is_negate = false
-  attribute_name = "User"
+  name            = "Cond1"
+  description     = "My description"
+  condition_type  = "LibraryConditionAttributes"
+  is_negate       = false
+  attribute_name  = "User"
   attribute_value = "User1"
   dictionary_name = "TACACS"
-  operator = "equals"
+  operator        = "equals"
 }
