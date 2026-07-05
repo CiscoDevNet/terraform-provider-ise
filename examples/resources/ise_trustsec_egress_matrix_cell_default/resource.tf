a6224fb9a52f3c76905796fa689e55a70b461006
camschae <camschae@cisco.com>
2026-07-04 20:34:44 -0400

    style: restore correct docs from main and run go generate ./...

--- examples/resources/ise_trustsec_egress_matrix_cell_default/resource.tf
+++ examples/resources/ise_trustsec_egress_matrix_cell_default/resource.tf
@@ -1,5 +1,5 @@
 resource "ise_trustsec_egress_matrix_cell_default" "example" {
-  description = "Default egress rule"
-  default_rule = "PERMIT_IP"
+  description        = "Default egress rule"
+  default_rule       = "PERMIT_IP"
   matrix_cell_status = "ENABLED"
 }
